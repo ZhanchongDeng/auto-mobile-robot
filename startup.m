a50b7b7c67cafbd2ecdc8c927a716576398e3323
Qian Meng <martinmq99@gmail.com>
2023-05-08 16:01:57 -0400

    dont need path

--- startup.m
+++
@@ -1,5 +0,0 @@
-folder_directory = pwd;
-folder_names = ["localization" "mapping" "planning" "/iRobotCreateSimulatorToolbox", "utils"];
-for name = folder_names
-    addpath(append(pwd,'/', name))
-end
